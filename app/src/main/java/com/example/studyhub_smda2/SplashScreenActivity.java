2e82a2b5626812eaa463ef80b55b4874775e5111
Umaima Kamran <167811217+UmaimaKamran73@users.noreply.github.com>
2026-03-18 10:35:43 +0500

    Splash Screen activity done (the logo icon yet to be edited out)

---
+++ app/src/main/java/com/example/studyhub_smda2/SplashScreenActivity.java
@@ -0,0 +1,63 @@
+package com.example.studyhub_smda2;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+@SuppressLint("CustomSplashScreen")
+public class SplashScreenActivity extends AppCompatActivity {
+
+    Animation fadein;
+    ImageView applogo;
+    TextView appname;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash_screen);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        init();
+        loadNextActivity();
+    }
+
+    private void init() {
+        applogo = findViewById(R.id.applogo);
+        appname = findViewById(R.id.appname);
+        loadAnimation();
+    }
+
+    private void loadAnimation() {
+        fadein = AnimationUtils.loadAnimation(this, R.anim.fadein);
+        applogo.startAnimation(fadein);
+        appname.startAnimation(fadein);
+
+    }
+
+    private void loadNextActivity()
+    {
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }, 4000);
+
+    }
+}
